2cd2a25c08784701975f37307060388145311f12
Truls Løvik <trulsaal@gmail.com>
2025-02-27 20:43:10 +0100

    Added Props

--- app/Components/ProfileImage.tsx
+++ app/Components/ProfileImage.tsx
@@ -1,12 +1,15 @@
 import Image from "next/image";
 import { getProfileImage } from "@/sanity/sanity-util";
+import { ReactNode } from "react";
 
 const profileImage = await getProfileImage();
-
+interface Props {
+  profileImage: ReactNode;
+}
 export default async function ProfileImage() {
   return (
     <div className="flex items-center md:relative">
-      {profileImage.map((profileImage) => (
+      {profileImage.map((profileImage: Props) => (
         <div
           key={profileImage._id}
           className="md:z-10 md:absolute md:overflow-x-clip md:overflow-y-clip object-cover md:overflow-hidden md:h-full md:-bottom-10 md:-right-10 bg-transparent my-auto w-56 px-2"
