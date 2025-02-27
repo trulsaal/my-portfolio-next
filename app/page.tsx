f5c762a5c21046dfecaa6f237a99b8e27654aa1e
Truls Løvik <trulsaal@gmail.com>
2025-02-27 20:35:12 +0100

    Established structure in HomePage

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,26 @@
 import { getProjects } from "@/sanity/sanity-util";
+import Container from "./Components/Container";
+import Header from "./Components/Upper/Header";
+import Hero from "./Components/Lower/Hero/Hero";
+import { PortableText } from "next-sanity";
+import { Outfit } from "next/font/google";
+import Image from "next/image";
+
+import Project from "./Components/Work-section/Project";
+const outfitFont = Outfit({ subsets: ["latin"], weight: "400" });
 
 export default async function Home() {
-  const projects = await getProjects();
   return (
-    <div>
-      {projects.map((project) => (
-        <div key={project._id}>{project.name}</div>
-      ))}
+    <div className={` flex flex-col md:gap-10 ${outfitFont.className}`}>
+      <Header></Header>
+      <Container>
+        <Hero />
+      </Container>
+      <section className="bg-slate-900">
+        <Container>
+          <Project></Project>
+        </Container>
+      </section>
     </div>
   );
 }
