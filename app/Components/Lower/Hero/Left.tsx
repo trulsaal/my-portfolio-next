47f9c68b970a95451bf1e114f5a3d6adc6e29603
Truls Løvik <trulsaal@gmail.com>
2025-03-04 19:48:01 +0100

    Clean up in unessesarry code

--- app/Components/Lower/Hero/Left.tsx
+++ app/Components/Lower/Hero/Left.tsx
@@ -1,37 +1,22 @@
 import { ReactNode } from "react";
 import ProfileImage from "../../ProfileImage";
-import ProfileImageTwo from "../../SimpleHero/ProfileImageTwo";
 
-interface Props {
-  children: ReactNode;
-}
-export default function Left({ children }: Props) {
+export default function Left() {
   return (
-    <div className="md:rounded-none rounded-2xl flex flex-col gap-16 bg-gradient-to-r  from-slate-100 from-0% py-4 to-white to-45%  w-full ">
-      <div className=" md:bg-[#d4d2d3] flex justify-between ">
-        <div className="flex px-2 md:px-0 rounded-2xl">{children}</div>
-        <div className="flex p-2">
-          <ProfileImageTwo />
-        </div>
+    <div className=" md:bg-[#d4d2d3] h-full px-16 flex justify-between relative">
+      <div className="pt-24 outline font-normal tracking-wide flex leading-7 w-80 text-2xl text-gray-800 ">
+        <span>
+          self-thought designer and front-end developer with passion for
+          <span className="text-gray-500 font-normal"> timeless</span>
+          <br />
+          and
+          <span className="text-gray-500 font-normal"> intuitive </span>
+          design.
+        </span>
       </div>
-      {/* <div className="px-2 grid grid-cols-2 space-x-2 h-fit">
-        <div className="">
-          <h1>Design</h1>
-          <p className="text-sm text-gray-600 leading-tight">
-            While the inner creative in me will be found making logos and brand
-            identeties, creating fluent user experience while staying modern and
-            fashionable the thing that I am committed to.
-          </p>
-        </div>
-        <div className="">
-          <h1 className="">Development</h1>
-          <p className="text-sm text-gray-600 leading-tight">
-            Fast, resilient solutions optimized for scale - built with
-            JavaScript and the right tools togehter with AI. Scalability and
-            performance are the core priorities in my work.
-          </p>
-        </div>
-      </div> */}
+      <div className="flex items-center absolute bottom-0 -right-20">
+        <ProfileImage></ProfileImage>
+      </div>
     </div>
   );
 }
--- app/Components/ProfileImage.tsx
+++ app/Components/ProfileImage.tsx
@@ -8,17 +8,14 @@ interface Props {
 }
 export default async function ProfileImage() {
   return (
-    <div className="flex items-center md:relative">
+    <div className="">
       {profileImage.map((profileImage: Props) => (
-        <div
-          key={profileImage._id}
-          className="md:z-10 md:absolute md:overflow-x-clip md:overflow-y-clip object-cover md:overflow-hidden md:h-full md:-bottom-10 md:-right-10 bg-transparent my-auto w-56 px-2"
-        >
+        <div key={profileImage._id} className="">
           <Image
-            className="rounded-full"
+            className="overflow-hidden"
             src={profileImage.image}
-            width={250}
-            height={250}
+            width={550}
+            height={500}
             alt={profileImage.alt}
           />
         </div>
