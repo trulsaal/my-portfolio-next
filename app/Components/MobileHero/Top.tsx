494c2544c7287dcf6c19d8142b17607fa6040f14
Truls Løvik <trulsaal@gmail.com>
2025-03-04 19:48:41 +0100

    Minor CSS changes

---
+++ app/Components/MobileHero/Top.tsx
@@ -0,0 +1,16 @@
+export default function Top() {
+  return (
+    <div className="md:hidden flex px-2 md:px-0 rounded-2xl">
+      <div className="items-center font-normal tracking-wide text-lg flex leading-none md:leading-5 md:text-2xl text-gray-600">
+        <span>
+          self-thought designer and front-end developer with passion for
+          <span className="text-gray-900 font-normal"> timeless</span>
+          <br />
+          and
+          <span className="text-gray-900 font-normal"> intuitive </span>
+          design.
+        </span>
+      </div>
+    </div>
+  );
+}
