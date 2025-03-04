1741b19f02d67a1246c02b7c54b0b0b358844620
Truls Løvik <trulsaal@gmail.com>
2025-03-04 19:49:28 +0100

    Fixed Haeder and added profile image

--- app/Components/Upper/Header.tsx
+++ app/Components/Upper/Header.tsx
@@ -1,6 +1,6 @@
 export default function Header() {
   return (
-    <div className="sticky w-full flex justify-between px-4 h-14 text-gray-400 items-center justify-items-end">
+    <div className="md:hidden sticky w-full flex justify-between px-4 h-14 text-gray-400 items-center justify-items-end">
       <div className=""></div>
       <div className="">Menu</div>
     </div>
