1c478527dc99cc08d170e92465a554eefffd808a
Truls Løvik <trulsaal@gmail.com>
2025-03-04 10:44:17 +0100

    Took away unused elements and imports

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,11 @@
-import { getProjects } from "@/sanity/sanity-util";
 import Container from "./Components/Container";
 import Header from "./Components/Upper/Header";
-import Hero from "./Components/Lower/Hero/Hero";
-import { PortableText } from "next-sanity";
+
 import { Outfit } from "next/font/google";
-import Image from "next/image";
 
 import Project from "./Components/Work-section/Project";
+import HeroTwo from "./Components/SimpleHero/HeroTwo";
+import WorkHeader from "./Components/Work-section/WorkHeader";
 const outfitFont = Outfit({ subsets: ["latin"], weight: "400" });
 
 export default async function Home() {
@@ -14,12 +13,12 @@ export default async function Home() {
     <div className={` flex flex-col md:gap-10 ${outfitFont.className}`}>
       <Header></Header>
       <Container>
-        <Hero />
+        <HeroTwo />
+        {/* <Hero /> */}
       </Container>
       <section className="bg-slate-900">
-        <Container>
-          <Project></Project>
-        </Container>
+        <WorkHeader></WorkHeader>
+        <Project></Project>
       </section>
     </div>
   );
